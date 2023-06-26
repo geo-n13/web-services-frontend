e3062f1351dede782686f7c19a108f3325dadc44
gh2o <geomercorepro@outlook.fr>
2023-06-26 16:41:34 +0200

    Avancement modale "Likés"

--- src/components/LikedMovieListManager.js
+++ src/components/LikedMovieListManager.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import '../styles/Manager.css';
+import CustomButton from './CustomButton';
+import { faCircleXmark,faFilm } from '@fortawesome/free-solid-svg-icons';
 
 const LikedMovieListManager = () => {
 
     return (
-        <div className="form-container">
-            <p>Hello !</p>
+        <div className="manager-list">
+            <div className="manager-list-item">
+                <p className="manager-title">Titre du film</p>
+                <p className="manager-year">Année de sortie</p>
+                <CustomButton label="Consulter" icon={faFilm} isModalButton={false} />
+                <CustomButton label="Supprimer" icon={faCircleXmark} isModalButton={false} />
+            </div>
         </div>
     );
 };
